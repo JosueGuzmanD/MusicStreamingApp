4f8e729a2dc0cd3e235a1e7302e4940b0bf8be00
Josue <josueg2001@gmail.com>
2025-03-07 23:27:03 +0100

    Album refactor & full testing

--- Core/Entities/Album.cs
+++ Core/Entities/Album.cs
@@ -4,10 +4,53 @@ namespace Core.Entities;
 
 public class Album : Entity
 {
-    public Guid ArtistId { get; set; }
-    public string Title { get; set; }
-    public Duration Duration { get; set; }
-    public string ImageUrl { get; set; }
+    public Guid ArtistId { get; private set; }
+    public string Title { get; private set; }
+    public string? ImageUrl { get; private set; }
+    public List<Song> Songs { get; private set; } 
+    
+    public Album(Guid artistId, string title, string? imageUrl = null)
+    {
+        if (artistId == Guid.Empty)
+            throw new ArgumentException("ArtistId cannot be empty.", nameof(artistId));
 
-    public List<Song> Songs { get; set; }
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title cannot be empty.", nameof(title));
+
+        ArtistId = artistId;
+        Title = title;
+        ImageUrl = imageUrl;
+        Songs = new List<Song>();
+    }
+    
+    public void AddSong(Song song)
+    {
+        if (song == null)
+            throw new ArgumentNullException(nameof(song), "Song cannot be null.");
+
+        Songs.Add(song);
+    }
+    public void UpdateImageUrl(string newImageUrl)
+    {
+        if (string.IsNullOrWhiteSpace(newImageUrl))
+            throw new ArgumentException("Image URL cannot be empty.", nameof(newImageUrl));
+
+        ImageUrl = newImageUrl;
+    }
+    public Duration GetAlbumDuration()
+    {
+        return CalculateDuration();
+    }
+
+    private Duration CalculateDuration()
+    {
+        var totalMinutes = Songs.Sum(s => s.Duration.Minutes);
+        var totalSeconds = Songs.Sum(s => s.Duration.Seconds);
+
+        totalMinutes += totalSeconds / 60;
+        totalSeconds %= 60;
+
+        return new Duration(totalMinutes, totalSeconds);
+    }
+
 }
---
+++ MusicStreamingAppTesting/DomainTesting/Entities/AlbumTests.cs
@@ -0,0 +1,112 @@
+﻿using Core.Entities;
+using Core.ValueObjects;
+using Shouldly;
+
+namespace MusicStreamingAppTesting.DomainTesting.Entities;
+
+public class AlbumTests
+{
+    [Fact]
+    public void Constructor_ShouldInitializeCorrectly_WhenValidDataIsProvided()
+    {
+        var artistId = Guid.NewGuid();
+        var title = "Test Album";
+        var imageUrl = "cover.jpg";
+
+        var album = new Album(artistId, title, imageUrl);
+
+        album.ArtistId.ShouldBe(artistId);
+        album.Title.ShouldBe(title);
+        album.ImageUrl.ShouldBe(imageUrl);
+        album.Songs.ShouldBeEmpty();
+    }
+
+
+    [Fact]
+    public void Constructor_ShouldThrowException_IfArtistIsNull()
+    {
+        var emptyArtistId = Guid.Empty;
+        var title = "Test Album";
+
+        var exception = Should.Throw<ArgumentException>(() => new Album(emptyArtistId, title));
+
+        exception.Message.ShouldContain("ArtistId cannot be empty.");
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowException_IfTitleIsNull()
+    {
+        var artistId = Guid.NewGuid();
+        var title = string.Empty;
+
+        var exception = Should.Throw<ArgumentException>(() => new Album(artistId, title));
+        exception.Message.ShouldContain("Title cannot be empty.");
+    }
+
+    [Fact]
+    public void AddSong_ShouldAddSong_IfIsCorrect()
+    {
+        var song = new Song();
+        var album = new Album(Guid.NewGuid(), "AlbumTitle");
+
+        album.AddSong(song);
+
+        album.Songs.ShouldContain(song);
+        album.Songs.Count.ShouldBe(1);
+    }
+
+    [Fact]
+    public void AddSong_ShouldThrowException_IfSongIsNull()
+    {
+        var album = new Album(Guid.NewGuid(), "AlbumTitle");
+        var exception = Should.Throw<ArgumentNullException>(() => album.AddSong(null));
+
+        exception.Message.ShouldContain("Song cannot be null.");
+        album.Songs.Count.ShouldBe(0);
+    }
+
+    [Fact]
+    public void GetAlbumDuration_ShouldReturnZero_WhenNoSongsAreAdded()
+    {
+        var album = new Album(Guid.NewGuid(), "Test Album");
+
+        var duration = album.GetAlbumDuration();
+
+        duration.Minutes.ShouldBe(0);
+        duration.Seconds.ShouldBe(0);
+    }
+    [Fact]
+    public void UpdateImageUrl_ShouldUpdateImageUrl_WhenValidUrlIsProvided()
+    {
+        var album = new Album(Guid.NewGuid(), "Test Album");
+        var newImageUrl = "new-cover.jpg";
+
+        album.UpdateImageUrl(newImageUrl);
+
+        
+        album.ImageUrl.ShouldBe(newImageUrl);
+    }
+
+    [Fact]
+    public void UpdateImageUrl_ShouldThrowArgumentException_WhenImageUrlIsEmpty()
+    {
+        var album = new Album(Guid.NewGuid(), "Test Album");
+
+        Should.Throw<ArgumentException>(() => album.UpdateImageUrl(""))
+            .Message.ShouldContain("Image URL cannot be empty.");
+    }
+    [Fact]
+    public void GetAlbumDuration_ShouldReturnCorrectDuration_WhenSongsAreAdded()
+    {
+        var album = new Album(Guid.NewGuid(), "Test Album");
+        album.AddSong(new Song { Duration = new Duration(3, 45) }); 
+        album.AddSong(new Song { Duration = new Duration(4, 30) }); 
+
+        var duration = album.GetAlbumDuration();
+
+        duration.Minutes.ShouldBe(8); 
+        duration.Seconds.ShouldBe(15); 
+    }
+    
+    
+}
