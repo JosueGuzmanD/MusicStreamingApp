397145064607e852766b647a326fa48aeb22f1b7
Josue <josueg2001@gmail.com>
2025-04-02 23:32:18 +0200

    DomainEvents

---
+++ Application/Interface/IDomainEventDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using Core.Interfaces;
+
+namespace Application.Interface;
+
+public interface IDomainEventDispatcher
+{
+    Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents);
+
+}
---
+++ Application/Interface/IDomainEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using Core.Interfaces;
+
+namespace Application.Interface;
+
+public interface IDomainEventHandler<T> where T : IDomainEvent
+{
+    Task Handle(T domainEvent);
+}
